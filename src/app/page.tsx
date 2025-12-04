e47c271ded0e9d577afa78555e9af29afc5b986e
Sahil Sajan <sahilsajjan18@gmail.com>
2025-12-04 17:56:15 +0500

    Revert "final build"
    This reverts commit c8695b777e86cdf535e7199e6834b70409aaa579.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+<<<<<<< Updated upstream
+import ListingCard from "@/components/cards";
+import Image from "next/image";
+import Hero from "./rent/Hero";
+=======
 const page = () => {
   return <div>page</div>;
 };
+>>>>>>> Stashed changes
 
 export default page;
