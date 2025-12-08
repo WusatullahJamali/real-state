1eed24f05276ccbc45f79ba0d0110faad0786d4e
s69073584-oss <s69073584@gmail.com>
2025-12-08 18:21:48 +0500

    Create PrivacyPolicy

---
+++ src/app/terms/page.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import TermsOfServices from '@/components/terms components/TermsOfServices'
+
+const page = () => {
+  return (
+    <div>
+      <TermsOfServices />
+    </div>
+  )
+}
+
+export default page
